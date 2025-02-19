038e798c9182344f5430a89bbafc960d53551e30
hyoni91 <hyoni.green@gmail.com>
2025-02-19 12:24:32 +0900

    비동기처리 수정

--- src/app/itemdetail/[id]/page.tsx
+++ src/app/itemdetail/[id]/page.tsx
@@ -1,8 +1,8 @@
 import ItemDetail from "@/components/ItemDetail";
 
-export default async function ItemDetailPage({ params }: { params: { id: string } }) {
-   const resolvedPrams = await params;
+export default  function ItemDetailPage({ params }: { params: { id: string } }) {
 
+    console.log(params.id);
 
-    return <ItemDetail itemId={resolvedPrams.id} />;
+    return <ItemDetail itemId={params.id} />;
 } 
