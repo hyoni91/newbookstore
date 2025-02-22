751ca1eb6ac374f1a0b00329a1a674bb63dc1d18
hyoni91 <hyoni.green@gmail.com>
2025-02-22 22:30:11 +0900

    배포 오류 점검

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  trailingSlash: true,
+  // trailingSlash: true,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
