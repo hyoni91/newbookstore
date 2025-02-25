3bb8a9e58df364e819a200d7841770b1358925c0
hyoni91 <hyoni.green@gmail.com>
2025-02-25 15:06:45 +0900

    데이터베이스 AWS과 연결

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   // trailingSlash: true,
   reactStrictMode: true,
+  env: {
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+    DB_PORT: process.env.DB_PORT,
+  },
 };
 
 export default nextConfig;
